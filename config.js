5957cc8207a814a83d374319dbebbe3cc101273e
hamouziGAMERS <47302859+hamouziGAMERS@users.noreply.github.com>
2019-02-10 19:04:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NTQ0MTk1MzkyMTY2OTUyOTYx.D0Hk_A.zjijKXhFBVUdvxHDfKkK1j0RX38'; // حط توكن البوت هنا
+
+exports.PREFIX = '1';//برفكس البوت هو يلي بيشغل لاغاني
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
